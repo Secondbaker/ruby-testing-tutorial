87eeb2d10b9c548aaed16373e84d058720c72f56
secondbaker <secondbaker@gmail.com>
2020-05-22 10:00:18 -0400

    Test now passes.

--- hotelier_spec.rb
+++ hotelier_spec.rb
@@ -8,9 +8,9 @@ class Hotel
         @occupied_rooms = []
     end
 
-    def check_in_guest(guest_name, room_number)
-        @guests << guest_name
+    def check_in_guest(guest_name, room_number)      
         return false if @occupied_rooms.include?(room_number)
+        @guests << guest_name
         @occupied_rooms << room_number
         true
     end
