2e30d09aca19b051ce5e708385830f8c9b4f058b
secondbaker <secondbaker@gmail.com>
2020-05-22 10:36:22 -0400

    I moved the tests into a separate file.  Not sure I'm satisfied with how I did it, since changing the file name or location would require me to update the testing file as well.  It's not really a problem in Rails, but I might figure this out later.

---
+++ hotelier.rb
@@ -0,0 +1,28 @@
+class Hotel
+    attr_accessor :guests
+
+    def initialize
+        @guests = []
+        @occupied_rooms = []
+    end
+
+    def check_in_guest(guest_name, room_number)      
+        return false if @occupied_rooms.include?(room_number)
+        @guests << guest_name
+        @occupied_rooms << room_number
+        true
+    end
+    
+    def check_out_guest(guest_name, room_number)
+        @guests.delete(guest_name)
+        @occupied_rooms.delete(room_number)
+    end
+
+    def room_is_vacant?(room_number)
+        !@occupied_rooms.include?(room_number)
+    end
+
+    def vacant_room_count
+
+    end
+end
