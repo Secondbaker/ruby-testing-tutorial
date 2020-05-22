6461b1bbc1b6b2ecb51795700c8766acf418a5a2
secondbaker <secondbaker@gmail.com>
2020-05-22 09:23:13 -0400

    Okay, I think that does it.  I like that this tutorial includes parts where you work ahead.  I do that anyway usually, as it's generally good practice for learning faster.

--- hotelier_spec.rb
+++ hotelier_spec.rb
@@ -10,6 +10,10 @@ class Hotel
     def check_in_guest(guest_name)
         @guests << guest_name
     end
+    
+    def check_out_guest(guest_name)
+
+    end
 end
 
 describe Hotel do 
@@ -17,5 +21,13 @@ describe Hotel do
         hotel = Hotel.new
         hotel.check_in_guest('George Harrison')
         expect(hotel.guests).to include 'George Harrison'
+    end
+
+    it 'can check out a guest' do
+        hotel = Hotel.new
+        hotel.check_in_guest('Ringo Starr')
+        expect(hotel.guests).to include 'Ringo Starr'
+        hotel.check_out_guest('Ringo Starr')
+        expect(hotel.guests).to_not include 'Ringo Starr'
     end
 end
