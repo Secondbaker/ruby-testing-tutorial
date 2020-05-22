44dd30d1c5d521053360d2055595ee9eff91cad4
secondbaker <secondbaker@gmail.com>
2020-05-22 09:35:03 -0400

    I went ahead and tried to write the tests Jason described.  Next I'll see how he implemented them.

--- hotelier_spec.rb
+++ hotelier_spec.rb
@@ -14,6 +14,10 @@ class Hotel
     def check_out_guest(guest_name)
         @guests.delete(guest_name)
     end
+
+    def vacant_room_count
+
+    end
 end
 
 describe Hotel do 
@@ -26,8 +30,22 @@ describe Hotel do
     it 'can check out a guest' do
         hotel = Hotel.new
         hotel.check_in_guest('Ringo Starr', 401)
-        expect(hotel.guests).to include 'Ringo Starr'
         hotel.check_out_guest('Ringo Starr')
         expect(hotel.guests).to_not include 'Ringo Starr'
+    end
+
+    it 'cannot check two guests into the same room' do
+        hotel = Hotel.new
+        hotel.check_in_guest('Masafumi Gotoh', 221)
+        hotel.check_in_guest('Ryuichi Sakamoto', 221)
+        expect(hotel.guests).to_not include 'Ryuichi Sakamoto'
+    end
+
+    it 'decreases total number of available rooms when a guest is checked in' do
+        hotel = Hotel.new
+        start_room_count = hotel.vacant_room_count
+        hotel.check_in_guest('Rivers Cuomo', 253)
+        end_room_count = hotel.vacant_room_count
+        expect(end_room_count).to eq(start_room_count - 1)
     end
 end
